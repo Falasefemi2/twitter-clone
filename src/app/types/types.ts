195b0dd0d0cbf3871c89a566837795bdf2015c70
Falase Femi <femifalase228@gmail.com>
2024-01-14 11:08:39 +0100

    feat: Short description
    Message body
    
    Message footer

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -6,3 +6,37 @@ export interface SidebarItemProps {
     icon: IconType;
     onClick?: () => void;
 }
+
+export interface HeaderProps {
+    label: string;
+    showBackArrow?: boolean;
+}
+
+export interface ButtonProps {
+    label: string;
+    secondary?:  boolean;
+    fullWidth?: boolean;
+    large?: boolean;
+    onClick: () => void;
+    disabled?: boolean;
+    outline?: boolean;
+}
+
+export interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled: boolean;
+}
+
+export interface InputProps {
+    placeholder?: string;
+    value?: string;
+    type?: string;
+    disabled?: boolean;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
