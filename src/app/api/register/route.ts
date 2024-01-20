266e283cabf337e68e4ab6ee2db8be716a67e1fb
Falase Femi <femifalase228@gmail.com>
2024-01-20 23:55:59 +0100

    fix(authetication): user can create an account

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,22 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]/options";
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import prisma from "../../libs/prismadb";
+import { NextResponse } from "next/server";
 
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email, username, name, password } = body;
 
-const handler = NextAuth(authOptions);
+  const hashedPassword = await bcrypt.hash(password, 12);
 
-export { handler as GET, handler as POST };
+  const user = await prisma?.user.create({
+    data: {
+      email,
+      username,
+      name,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user)
+}
