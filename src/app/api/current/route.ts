0555633c128296698473b893eb3062ed50e6fd70
Falase Femi <femifalase228@gmail.com>
2024-01-21 13:03:44 +0100

    ⨯ Detected default export in 'C:\Users\HP\twitter-clone\src\app\api\current\route.ts'. Export a named export for each HTTP method instead.

--- src/app/api/current/route.ts
+++ src/app/api/current/route.ts
@@ -1,16 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import serverAuth from "../../libs/serverAuth";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "GET") {
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
     return res.status(405).end();
   }
 
   try {
-    const { currentUser } = await serverAuth(req);
+    const { currentUser } = await serverAuth(req, res);
 
     return res.status(200).json(currentUser);
   } catch (error) {
@@ -18,3 +17,5 @@ export default async function handler(
     return res.status(400).end();
   }
 }
+
+
